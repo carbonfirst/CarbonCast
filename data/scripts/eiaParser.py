8de82836caffb8893590ae9fc4046000c63b09fb
Rohan Lekhwani <rohanlekhwani@ymail.com>
2023-03-04 12:53:42 -0500

    feat: automate electricity data fetch from all regions

---
+++ data/scripts/eiaParser.py
@@ -0,0 +1,68 @@
+import os
+import requests
+import pandas as pd
+
+EIA_API_KEY="CZdQsisRJzwOfqUWV3jiMPNEx3ZbHcuJ2VQus04i"
+EIA_SOURCE_MAP = {
+    "OTH": "other", 
+    "COL": "coal",
+    "SUN": "solar",
+    "NG": "nat_gas",
+    "NUC": "nuclear",
+    "WND": "wind",
+    "WAT": "hydro",
+    "OIL": "oil"
+    }
+
+EIA_BAL_AUTH_LIST = [ 'CISO', 'FPL', 'ISNE', 'NYISO', 'PJM', 'ERCO', 'BPAT']
+
+
+def getProductionDataBySourceTypeDataFromEIA(ba):
+    print(ba)
+    API_URL="https://api.eia.gov/v2/electricity/rto/fuel-type-data/data?api_key="
+    API_URL_SORT_PARAMS="sort[0][column]=period&sort[0][direction]=asc&sort[1][column]=fueltype&sort[1][direction]=desc"
+    API_URL_SUFFIX="&frequency=hourly&data[]=value&facets[respondent][]={}&"+API_URL_SORT_PARAMS+"&start={}&end={}&offset=0&length=5000"
+    startTime = "2023-03-03T00"
+    endTime = "2023-03-03T23"
+    URL = API_URL+EIA_API_KEY+API_URL_SUFFIX.format(ba, startTime, endTime)
+    resp = requests.get(URL)
+    print(resp.url)
+    if (resp.status_code != 200):
+        print("Error! Code: ", resp.status_code)
+        print("Error! Message: ", resp.text)
+        print("Error! Reason: ", resp.reason)
+    responseData = resp.json()["response"]["data"]
+    return responseData, startTime
+
+def parseEIAProductionDataBySourceType(data, startTime):
+    datasetColumns = []
+    datasetColumns.append("UTC time")
+    numSources = 0
+    electricityBySource = []
+    electricityProductionData = []
+    electricityBySource.append(startTime)
+
+    for electricitySourceData in data:
+        if (electricitySourceData["period"] == startTime):
+            numSources += 1
+            datasetColumns.append(EIA_SOURCE_MAP[electricitySourceData["fueltype"]])
+            electricityBySource.append(electricitySourceData["value"])
+        else:
+            if len(electricityBySource) == numSources + 1:
+                electricityProductionData.append(electricityBySource)
+                electricityBySource = [electricitySourceData["period"]]
+            electricityBySource.append(electricitySourceData["value"])
+    electricityProductionData.append(electricityBySource)
+
+    dataset = pd.DataFrame(electricityProductionData, columns=datasetColumns)
+    return dataset, numSources
+
+if __name__ == "__main__":
+    for balAuth in EIA_BAL_AUTH_LIST:
+        data, startTime = getProductionDataBySourceTypeDataFromEIA(balAuth)
+        dataset, numSources = parseEIAProductionDataBySourceType(data, startTime)
+        print(dataset)
+        script_dir = os.path.abspath('.')
+        csv_path = os.path.join(script_dir, f"data/{balAuth}/day/{startTime[:-3]}.csv")
+        with open(csv_path, 'w') as f:
+            dataset.to_csv(f, index=False)
