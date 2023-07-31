ae4f522f9c16773a11e46aacd7489675f1e2ce03
Chae Kim <chaekim0820@gmail.com>
2023-07-31 01:13:41 -0400

    source code parsing raw data (for  missing data)

---
+++ src/entsoeRawDataParser.py
@@ -0,0 +1,112 @@
+import os
+import pandas as pd
+from entsoe import EntsoePandasClient
+from datetime import datetime, timedelta
+import time
+import numpy as np
+import sys
+
+# public key for ENTSOE API
+ENTSOE_API_KEY="c0b15cbf-634c-4884-b784-5b463182cc97"
+
+ENTSOE_BAL_AUTH_LIST = ['AT', 'BE', 'BG', 'HR', 'CZ', 'DK', 'EE', 'FI', 
+                         'FR', 'DE', 'GB', 'GR', 'HU', 'IE', 'IT', 'LV', 'LT', 'NL',
+                        'PL', 'PT', 'RO', 'RS', 'SK', 'SI', 'ES', 'SE', 'CH']
+# ENTSOE_BAL_AUTH_LIST =  ['AT', 'BE', 'BG', 'HR', 'CZ', 'EE', 'FI', 
+#                         'GB', 'GR', 'HU', 'IE', 'IT', 'LV', 'LT', 'NL',
+#                         'PL', 'PT','RS', 'SK', 'SI', 'ES', 'SE', 'CH']
+INVALID_AUTH_LIST = ['AL', 'DK-DK2']
+
+# get production data by source type from ENTSOE API
+def getProductionDataBySourceTypeDataFromENTSOE(ba, curDate, curEndDate):
+    print(ba)
+
+    startDate = pd.Timestamp(curDate, tz='UTC')
+    endDate = pd.Timestamp(curEndDate, tz='UTC') + pd.Timedelta(hours=23, minutes=45)
+    print(startDate, endDate)
+
+    client = EntsoePandasClient(api_key=ENTSOE_API_KEY)
+    try:
+        dataset = client.query_generation(ba, start=startDate, end=endDate, psr_type=None)
+        empty = False
+    except: # mark if data was empty
+        dataset = None
+        empty = True
+
+    return dataset, empty
+
+# parse production data by source type from ENTSOE API
+def parseENTSOEProductionDataBySourceType(data):  
+    dataset = pd.DataFrame()
+    timerows = []
+    for column in data: # iterate through the columns
+        if (type(column) is tuple):
+            source = column[0]
+        else:
+            source = column
+        if (source == "Hydro Pumped Storage"):
+            continue # Ignoring storage for now
+        if (source in dataset.columns): # fix; find out how to keep nan values
+            # dataset[source] = dataset[source].fillna(0) + data[column].fillna(0)
+            for row in range(len(data.index)):
+                firstNaN = pd.isna(dataset[source].iloc[row])
+                secondNaN = pd.isna(data[column].iloc[row])
+                if (firstNaN and secondNaN):
+                    dataset[source].iloc[row] = np.nan
+                else:
+                    firstVal = 0 if firstNaN else dataset[source].iloc[row]
+                    secondVal = 0 if secondNaN else data[column].iloc[row]
+                    dataset[source].iloc[row] = firstVal + secondVal
+        else:
+            dataset.insert(loc=len(dataset.columns), column=source, value=data[column])
+    # converting time to UTC time & appending as 1st column
+    for time in data.index:
+        timerows.append(time.astimezone(tz='UTC'))
+    dataset.insert(loc=0, column="UTC Time", value=timerows)
+    return dataset
+
+def getElectricityProductionDataFromENTSOE(balAuth, startDate, numDays, DAY_JUMP):
+    fullDataset = pd.DataFrame()
+    startDateObj = datetime.strptime(startDate, "%Y-%m-%d")
+
+    for days in range(0, numDays, DAY_JUMP): # DAY_JUMP: # days of data got each time
+        endDateObj = startDateObj + timedelta(days=DAY_JUMP-1)
+        endDate = endDateObj.strftime("%Y-%m-%d")
+        data, empty = getProductionDataBySourceTypeDataFromENTSOE(balAuth, startDate, endDate)
+        print("was the dataset empty?: ", empty)
+
+        if (empty):
+            continue # skip this iteration
+        dataset = parseENTSOEProductionDataBySourceType(data)
+        
+        if (days == 0):
+            fullDataset = dataset.copy()
+        else:
+            if (days%60 == 0):
+                time.sleep(1)
+            fullDataset = pd.concat([fullDataset, dataset], sort=False) # if doesn't work for new columns, fix here
+
+        # startDate incremented    
+        startDateObj = startDateObj + timedelta(days=DAY_JUMP)
+        startDate = startDateObj.strftime("%Y-%m-%d")
+
+    return fullDataset
+
+if __name__ == "__main__":
+    if (len(sys.argv)!=3):
+        print("Usage: python3 entsoeParser.py <yyyy-mm-dd> <# days to fetch data for>")
+        exit(0)
+    startDate = sys.argv[1] # "2022-01-01" #"2019-01-01"
+    numDays = int(sys.argv[2]) #368 #1096
+
+    for balAuth in ENTSOE_BAL_AUTH_LIST:
+        fullDataset = getElectricityProductionDataFromENTSOE(balAuth, startDate, numDays, DAY_JUMP=8)
+
+        parentdir = os.path.normpath(os.path.join(os.getcwd(), os.pardir)) # goes to CarbonCast folder
+        filedir = os.path.normpath(os.path.join(parentdir, f"./data/EU_DATA/{balAuth}/ENTSOE"))
+        
+        csv_path = os.path.normpath(os.path.join(filedir, f"./{balAuth}_raw_original_sources.csv"))
+        with open(csv_path, 'w') as f: # open as f means opens as file
+            fullDataset.to_csv(f, index=False)
+
+        print("raw data parsed for " + balAuth)
