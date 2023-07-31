c197c5b7f384429506db47d068f8cdf8326c82c0
Chae Kim <chaekim0820@gmail.com>
2023-07-31 01:52:12 -0400

    added for forecast data

--- src/entsoeRawDataParser.py
+++ src/entsoeRawDataParser.py
@@ -9,12 +9,13 @@ import sys
 # public key for ENTSOE API
 ENTSOE_API_KEY="c0b15cbf-634c-4884-b784-5b463182cc97"
 
-ENTSOE_BAL_AUTH_LIST = ['AT', 'BE', 'BG', 'HR', 'CZ', 'DK', 'EE', 'FI', 
-                         'FR', 'DE', 'GB', 'GR', 'HU', 'IE', 'IT', 'LV', 'LT', 'NL',
-                        'PL', 'PT', 'RO', 'RS', 'SK', 'SI', 'ES', 'SE', 'CH']
+# ENTSOE_BAL_AUTH_LIST = ['AT', 'BE', 'BG', 'HR', 'CZ', 'DK', 'EE', 'FI', 
+#                          'FR', 'DE', 'GB', 'GR', 'HU', 'IE', 'IT', 'LV', 'LT', 'NL',
+#                         'PL', 'PT', 'RO', 'RS', 'SK', 'SI', 'ES', 'SE', 'CH']
 # ENTSOE_BAL_AUTH_LIST =  ['AT', 'BE', 'BG', 'HR', 'CZ', 'EE', 'FI', 
 #                         'GB', 'GR', 'HU', 'IE', 'IT', 'LV', 'LT', 'NL',
 #                         'PL', 'PT','RS', 'SK', 'SI', 'ES', 'SE', 'CH']
+ENTSOE_BAL_AUTH_LIST = ['DK', 'DE', 'FR', 'RO']
 INVALID_AUTH_LIST = ['AL', 'DK-DK2']
 
 # get production data by source type from ENTSOE API
@@ -92,6 +93,77 @@ def getElectricityProductionDataFromENTSOE(balAuth, startDate, numDays, DAY_JUMP
 
     return fullDataset
 
+
+
+
+# for FORECAST raw data
+
+# get forecast data by source type from ENTSOE API
+def getForecastDataBySourceTypeDataFromENTSOE(ba, curDate, curEndDate):
+    print(ba)
+
+    startDate = pd.Timestamp(curDate, tz='UTC')
+    endDate = pd.Timestamp(curEndDate, tz='UTC') + pd.Timedelta(hours=23, minutes=45)
+    print(startDate, endDate)
+
+    client = EntsoePandasClient(api_key=ENTSOE_API_KEY)
+    try:
+        dataset = client.query_wind_and_solar_forecast(ba, start=startDate, end=endDate, psr_type=None)
+        empty = False
+    except: # mark if data was empty
+        dataset = None
+        empty = True
+
+    return dataset, empty
+
+# parse forecast data by source type from ENTSOE API
+def parseENTSOEForecastDataBySourceType(data):  
+    dataset = pd.DataFrame()
+    timerows = []
+    for column in data: # iterate through the columns
+        if (type(column) is tuple):
+            source = column[0]
+        else:
+            source = column
+        if (source in dataset.columns): # fix; find out how to keep nan values
+            print("there is a duplicate for source ", source)
+            exit()
+        dataset.insert(loc=len(dataset.columns), column=source, value=data[column], allow_duplicates=True)
+    # converting time to UTC time & appending as 1st column
+    for time in data.index:
+        timerows.append(time.astimezone(tz='UTC'))
+    dataset.insert(loc=0, column="UTC Time", value=timerows)
+    return dataset
+
+def getElectricityForecastDataFromENTSOE(balAuth, startDate, numDays, DAY_JUMP):
+    fullDataset = pd.DataFrame()
+    startDateObj = datetime.strptime(startDate, "%Y-%m-%d")
+
+    for days in range(0, numDays, DAY_JUMP): # DAY_JUMP: # days of data got each time
+        endDateObj = startDateObj + timedelta(days=DAY_JUMP-1)
+        endDate = endDateObj.strftime("%Y-%m-%d")
+        data, empty = getForecastDataBySourceTypeDataFromENTSOE(balAuth, startDate, endDate)
+        print("was the dataset empty?: ", empty)
+
+        if (empty):
+            continue # skip this iteration
+        dataset = parseENTSOEForecastDataBySourceType(data)
+        
+        if (days == 0):
+            fullDataset = dataset.copy()
+        else:
+            if (days%60 == 0):
+                time.sleep(1)
+            fullDataset = pd.concat([fullDataset, dataset], sort=False) # if doesn't work for new columns, fix here
+
+        # startDate incremented    
+        startDateObj = startDateObj + timedelta(days=DAY_JUMP)
+        startDate = startDateObj.strftime("%Y-%m-%d")
+
+    return fullDataset
+
+
+
 if __name__ == "__main__":
     if (len(sys.argv)!=3):
         print("Usage: python3 entsoeParser.py <yyyy-mm-dd> <# days to fetch data for>")
@@ -100,13 +172,18 @@ if __name__ == "__main__":
     numDays = int(sys.argv[2]) #368 #1096
 
     for balAuth in ENTSOE_BAL_AUTH_LIST:
-        fullDataset = getElectricityProductionDataFromENTSOE(balAuth, startDate, numDays, DAY_JUMP=8)
+        # fullProductionDataset = getElectricityProductionDataFromENTSOE(balAuth, startDate, numDays, DAY_JUMP=8)
+        fullForecastDataset = getElectricityForecastDataFromENTSOE(balAuth, startDate, numDays, DAY_JUMP=8)
 
         parentdir = os.path.normpath(os.path.join(os.getcwd(), os.pardir)) # goes to CarbonCast folder
         filedir = os.path.normpath(os.path.join(parentdir, f"./data/EU_DATA/{balAuth}/ENTSOE"))
         
-        csv_path = os.path.normpath(os.path.join(filedir, f"./{balAuth}_raw_original_sources.csv"))
+        # csv_path = os.path.normpath(os.path.join(filedir, f"./{balAuth}_raw_production.csv"))
+        # with open(csv_path, 'w') as f: # open as f means opens as file
+        #     fullProductionDataset.to_csv(f, index=False)
+
+        csv_path = os.path.normpath(os.path.join(filedir, f"./{balAuth}_raw_forecast.csv"))
         with open(csv_path, 'w') as f: # open as f means opens as file
-            fullDataset.to_csv(f, index=False)
+            fullForecastDataset.to_csv(f, index=False)
 
         print("raw data parsed for " + balAuth)
