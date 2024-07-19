5a89ca23729690000f08f89b5c50fdb37ca771fe
Jason Chen <jc@vl965-172-31-203-210.wireless.umass.edu>
2024-07-19 17:31:55 -0400

    bug fixes for new data format

--- src/dailyFetcherEU.py
+++ src/dailyFetcherEU.py
@@ -8,13 +8,31 @@ import seaborn as sns
 import sys
 import os 
 
-EU_REGIONS = ["AT","BE","BG","CH","CZ","DE","DK","EE","ES","FI",
-              "FR","GB","GR","HR","HU","IE","IT","LT","LV","NL",
-              "PL","PT","RO","RS","SE","SI","SK"]
+#EU_REGIONS = ["AT","BE","BG","CH","CZ","DK","EE","ES","FI",
+#             "FR","GB","GR","HR","HU","IE","IT","LT","LV","NL",
+#            "PL","PT","RO","RS","SE","SI","SK"] # testing DE right now don't upload 
 
-FILES = ["clean.csv", "direct_emissions.csv", "lifecycle_emissions.csv",
-                  "96hr_forecasts_DA.csv", "direct_96hr_CI_forecasts_0.csv", 
-                  "lifecycle_96_hr_CI_forecasts_0.csv"]
+EU_REGIONS = ['BE']
+
+FILES = [
+    "clean.csv", 
+    "direct_emissions.csv", 
+    "lifecycle_emissions.csv",
+    "96hr_forecasts_DA.csv",
+    "direct_96hr_CI_forecasts_0.csv",
+    "direct_96hr_CI_forecasts_0PERIOD_0.csv",
+    "direct_96hr_CI_forecasts_0PERIOD_1.csv",
+    "direct_96hr_CI_forecasts_0PERIOD_2.csv",
+    "lifecycle_96hr_CI_forecasts_0.csv",
+    "lifecycle_96hr_CI_forecasts_0PERIOD_0.csv",
+    "lifecycle_96hr_CI_forecasts_0PERIOD_1.csv",
+    "lifecycle_96hr_CI_forecasts_0PERIOD_2.csv"
+]
+version = ["3.1"]*96
+creationTime = ["2024-07-17 00:00:00"]*96
+
+version_2 = ["3.1"]*24
+creationTime_2 = ["2024-07-17 00:00:00"]*24
 
 def getDailyFilesFromDataFile(folder,source,region_name,file_name):
     dataset = pd.read_csv(f"../{folder}/{source}/{region_name}/{file_name}", header = 0)
@@ -40,20 +58,31 @@ def getDailyFilesFromDataFile(folder,source,region_name,file_name):
         start = i
         end = min(i+numRows, len(dataset))
         subset = dataset.iloc[start:end]
+        subset.rename(columns={"datetime": "UTC time"}, inplace=True)
         curDate = pd.to_datetime(dataset[index].values[i]).strftime('%Y-%m-%d')
         print(curDate)
         # print(subset)
         if file_name == f"{region_name}_clean.csv":
             output_file_name = f"{region_name}_{curDate}.csv"
         elif file_name.endswith("_direct_emissions.csv"):
+            subset.insert(2, "creation_time (UTC)", creationTime_2)
+            subset.insert(3, "version", version_2)
             output_file_name = f"{region_name}_{curDate}_direct_emissions.csv"
         elif file_name.endswith("_lifecycle_emissions.csv"):
+            subset.insert(2, "creation_time (UTC)", creationTime_2)
+            subset.insert(3, "version", version_2)
             output_file_name = f"{region_name}_{curDate}_lifecycle_emissions.csv"
         elif file_name.endswith("_96hr_forecasts_DA.csv"):
+            subset.insert(1, "creation_time (UTC)", creationTime)
+            subset.insert(2, "version", version)
             output_file_name = f"{region_name}_96hr_forecasts_{curDate}.csv"
-        elif file_name.endswith("_direct_96hr_CI_forecasts_0.csv"):
+        elif file_name.endswith(("_direct_96hr_CI_forecasts_0PERIOD_0.csv","_direct_96hr_CI_forecasts_0PERIOD_1.csv","_direct_96hr_CI_forecasts_0PERIOD_2.csv")):
+            subset.insert(1, "creation_time (UTC)", creationTime)
+            subset.insert(2, "version", version)
             output_file_name = f"{region_name}_direct_CI_forecasts_{curDate}.csv"
         else:
+            subset.insert(1, "creation_time (UTC)", creationTime)
+            subset.insert(2, "version", version)
             output_file_name = f"{region_name}_lifecycle_CI_forecasts_{curDate}.csv"
         subset.to_csv(f"{outputFileDir}/{output_file_name}", index=False)
 
@@ -77,7 +106,7 @@ if __name__ == "__main__":
             if file in ["clean.csv", "direct_emissions.csv", "lifecycle_emissions.csv", "96hr_forecasts_DA.csv"]:
                 folder = "data"
             else:
-                folder = "CI_forecast_Data"
+                folder = "CI_forecast_data"
             file_path = f"../{folder}/{source}/{region_name}/{file_name}"
             if os.path.exists(file_path):
                 print(f"Processing region: {region_name}, file: {file_name}")
