453bd2da87d17c89ec41163c50604c00ac8550e2
Abel Souza <abelpc_uff@yahoo.com.br>
2024-07-20 01:17:38 -0400

    Directory option

--- real_time/data.sh
+++ real_time/data.sh
@@ -3,8 +3,12 @@
 # Change the below to the CarbonCast diretory
 CARBON_CAST_DIR=.
 
+# Change the below to the directory you want to move
+# Default is `EU_DATA`
+DATA=EU_DATA
+
 # No need to change the below
-DIR_SRC=$CARBON_CAST_DIR/EU_DATA
+DIR_SRC=$CARBON_CAST_DIR/$DATA
 DIR_DST=$CARBON_CAST_DIR/real_time
 
 pushd $DIR_SRC
