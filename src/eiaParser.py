2485a5a5419cb1e67d1883d03d4fcd531d73ce08
Patrick He <he.patrick2005@gmail.com>
2025-03-04 00:08:33 -0500

    Fueltype check

--- src/eiaParser.py
+++ src/eiaParser.py
@@ -96,7 +96,9 @@ def parseEIAProductionDataBySourceType(data, startDate, electricitySources, numS
 
     for electricitySourceData in data:
         if (electricitySourceData["period"] == curDate):
-            electricityBySource[EIA_SOURCE_MAP[electricitySourceData["fueltype"]]]= electricitySourceData["value"]
+            fueltype = electricitySourceData["fueltype"]
+            if fueltype in EIA_SOURCE_MAP.keys():
+                electricityBySource[EIA_SOURCE_MAP[fueltype]]= electricitySourceData["value"]
         else:
             curDate = electricitySourceData["period"]
             curHour = curDate.split("T")[1]
@@ -108,7 +110,9 @@ def parseEIAProductionDataBySourceType(data, startDate, electricitySources, numS
             electricityProductionData.append(hourlyElectricityData)
             # print(hourlyElectricityData)
             hourlyElectricityData = [curDate.split("T")[0]+" "+curHour.zfill(2)+":00"]
-            electricityBySource[EIA_SOURCE_MAP[electricitySourceData["fueltype"]]]= electricitySourceData["value"]
+            fueltype = electricitySourceData["fueltype"]
+            if fueltype in EIA_SOURCE_MAP.keys():
+                electricityBySource[EIA_SOURCE_MAP[fueltype]]= electricitySourceData["value"]
     for source in electricitySources:
         hourlyElectricityData.append(electricityBySource[source])
     electricityProductionData.append(hourlyElectricityData)
@@ -143,7 +147,9 @@ def getElectricityProductionDataFromEIA(balAuth, startDate, numDays, DAY_JUMP):
             return fullDataset
         if (days == 0): # assuming all data is correctly available for the first day at least
             for electricitySourceData in data:
-                electricitySources.add(EIA_SOURCE_MAP[electricitySourceData["fueltype"]])
+                fueltype = electricitySourceData["fueltype"]
+                if fueltype in EIA_SOURCE_MAP.keys():
+                    electricitySources.add(EIA_SOURCE_MAP[fueltype])
                 numSources = len(electricitySources)
         dataset = parseEIAProductionDataBySourceType(data, startDate, electricitySources, numSources)
         print(dataset)
